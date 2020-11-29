399b76b7cc2191a1af9a40ec1971aab17de52729
l.medovnikov <l.medovnikov@cuberto.ru>
2020-11-29 17:40:06 +0300

    forgot about simplifying onCreateView()

--- app/src/main/java/com/honeybeard/androidfundamentals/activities/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/com/honeybeard/androidfundamentals/activities/movie_details/MovieDetailsFragment.kt
@@ -1,20 +1,11 @@
 package com.honeybeard.androidfundamentals.activities.movie_details
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.honeybeard.androidfundamentals.R
 
-class MovieDetailsFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
-    }
+class MovieDetailsFragment : Fragment(R.layout.fragment_movie_details) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/honeybeard/androidfundamentals/activities/movie_list/MoviesListFragment.kt
+++ app/src/main/java/com/honeybeard/androidfundamentals/activities/movie_list/MoviesListFragment.kt
@@ -2,21 +2,12 @@ package com.honeybeard.androidfundamentals.activities.movie_list
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.honeybeard.androidfundamentals.R
 
-class MoviesListFragment : Fragment() {
+class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
     private var selectMovieListener: SelectMovieListener? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movies_list, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
